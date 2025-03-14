5bee173699a1630fd958bf7df193ede6e579c2a2
lordbird12 <pu.niwat_st@tni.ac.th>
2025-03-14 17:37:31 +0700

    aaa

--- src/app/modules/admin/sales/page.service.ts
+++ src/app/modules/admin/sales/page.service.ts
@@ -342,9 +342,11 @@ export class PageService {
             );
     }
 
-    getMachineModelById(id): Observable<any> {
+    getMachineModelByTeam(id): Observable<any> {
+        var user = JSON.parse(localStorage.getItem('user'));
+        console.log(user);
         return this._httpClient
-            .get<any>(environment.baseURL + '/api/get_machine_model/'+id)
+            .get<any>(environment.baseURL + '/api/get_machine_model_team'+id)
             .pipe(
                 tap((result) => {
                     this._data.next(result);
